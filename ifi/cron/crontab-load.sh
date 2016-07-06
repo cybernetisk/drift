c7c882a0ef4b878549213c2cec8aee505810c2be
Henrik Steen <henrist@henrist.net>
2016-07-06 11:44:09 +0000

    Fix erroneous use of backticks

--- ifi/cron/crontab-load.sh
+++ ifi/cron/crontab-load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $(id -nu) != cyb ] || [ $(hostname) != vor.ifi.uio.no ]; then
-    "Must be user `cyb` running on `vor.ifi.uio.no` to use this script"
+    "Must be user 'cyb' running on 'vor.ifi.uio.no' to use this script"
     exit 1
 fi
 
