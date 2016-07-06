3e34c688f15057a76370ac4f386be6a382af5ced
Henrik Steen <henrist@henrist.net>
2016-07-06 11:22:46 +0200

    Add cron-related files used on Ifi

---
+++ ifi/cron/crontab-save.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $(id -nu) != cyb ] || [ $(hostname) != vor.ifi.uio.no ]; then
+    "Must be user `cyb` running on `vor.ifi.uio.no` to use this script"
+    exit 1
+fi
+
+crontab -l >crontab
