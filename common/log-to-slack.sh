09c205c3299051a2b05968687c385a1c969013a3
Henrik Steen <henrist@henrist.net>
2016-07-07 12:15:21 +0000

    Fix log-to-slack script
    The issue was that the bash functions only read one line,
    and that the pipe was not connected between stdin of the
    bash functions and stdin of slacktee, so if we read multiple
    lines they would be passed line-by-line to slacktee.
    
    Just solves all this by removing the bash functions,
    as they are really not needed.

--- common/log-to-slack.sh
+++ common/log-to-slack.sh
@@ -7,18 +7,4 @@ if [ $(id -un) == "cyb" ]; then
     slacktee=~/.local/bin/slacktee.sh
 fi
 
-HandleOut() {
-    read data
-    if [ -n "$data" ]; then
-        echo -n "$data" | $slacktee -u "$(hostname)" -c drift-stdout -t "$title" >/dev/null
-    fi
-}
-
-HandleErr() {
-    read data
-    if [ -n "$data" ]; then
-        echo -n "$data" | $slacktee -u "$(hostname)" -c drift-stderr -t "$title" >/dev/null
-    fi
-}
-
-(eval $@ | HandleOut) 2>&1 | HandleErr
+(eval $@ | $slacktee -u "$(hostname)" -c drift-stdout -t "$title" >/dev/null) 2>&1 | $slacktee -u "$(hostname)" -c drift-stderr -t "$title" >/dev/null
