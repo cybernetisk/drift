a573fc55bb643ddf56d0956178a802e37ad34115
Henrik Steen <henrist@henrist.net>
2016-07-20 18:59:59 +0000

    Docker setup for okoreports

--- nginx-front/run.sh
+++ nginx-front/run.sh
@@ -6,5 +6,7 @@ docker run \
     --restart always \
     --name cyb-nginx-front \
     -v nginx-certs:/opt/letsencrypt.sh/certs \
+    -v okoreports-reports:/var/www/okoreports-reports \
+    -v okoreports-frontend-dist:/var/www/okoreports-frontend-dist \
     -p 80:80 -p 443:443 \
     cyb/nginx-front
