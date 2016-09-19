4d662c02cc81a1218cbb45f8a9d7d74b7e9009df
Henrik Steen <henrist@henrist.net>
2016-09-19 18:25:26 +0000

    nginx-front: Use newer and improved docker image

--- nginx-front/run.sh
+++ nginx-front/run.sh
@@ -5,13 +5,18 @@ docker run \
     --net cyb \
     --restart always \
     --name cyb-nginx-front \
+    -e LE_CONTACT_EMAIL=drift@cyb.no \
     -v internsystem-backend-static:/var/www/internsystem-backend-static \
     -v internsystem-backend-static-test:/var/www/internsystem-backend-static-test \
     -v internsystem-frontend-dist:/var/www/internsystem-frontend-dist \
     -v internsystem-frontend-dist-test:/var/www/internsystem-frontend-dist-test \
     -v internsystem-frontend-dist-test-frontend:/var/www/internsystem-frontend-dist-test-frontend \
-    -v nginx-certs:/opt/letsencrypt.sh/certs \
+    -v letsencrypt-certs:/opt/letsencrypt.sh/certs \
+    -v letsencrypt-accounts:/opt/letsencrypt.sh/accounts \
     -v okoreports-reports:/var/www/okoreports-reports \
     -v okoreports-frontend-dist:/var/www/okoreports-frontend-dist \
+    -v "$(pwd)/nginx/nginx.conf":/etc/nginx/nginx.conf \
+    -v "$(pwd)/nginx/okoreports.htpasswd":/etc/nginx/okoreports.htpasswd \
+    -v "$(pwd)/nginx/conf.d":/etc/nginx/conf.d \
     -p 80:80 -p 443:443 \
-    cyb/nginx-front
+    henrist/nginx-letsencrypt
