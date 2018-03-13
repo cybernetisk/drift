1540d26cc05ade82858a4127b0ed6cf397c2b121
Thor K. H <thor@roht.no>
2018-03-13 18:34:48 +0100

    Updated to support BusyBox stat

--- confluence/container/docker-entrypoint-cyb.sh
+++ confluence/container/docker-entrypoint-cyb.sh
@@ -5,7 +5,7 @@
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
-if [ "$(stat --format "%Y" "${CONF_INSTALL}/conf/server.xml")" -eq "0" ]; then
+if [ "$(stat -c "%Y" "${CONF_INSTALL}/conf/server.xml")" -eq "0" ]; then
   if [ -n "${X_CROWD_PASSWORD}" ]; then
     sed "s/PASSWORD_TO_BE_REPLACED/${X_CROWD_PASSWORD}/" "${CONF_INSTALL}/confluence/WEB-INF/classes/crowd.properties" >/tmp/crowd.properties
     cat /tmp/crowd.properties >"${CONF_INSTALL}/confluence/WEB-INF/classes/crowd.properties"
