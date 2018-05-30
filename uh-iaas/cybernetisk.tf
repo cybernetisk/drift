3578cc0f47032d4ff0f1b05fc5d2bf1e48266987
Adrian Helle <adriah@ulrik.uio.no>
2018-05-30 18:53:36 +0200

    Add SSH

--- uh-iaas/cybernetisk.tf
+++ uh-iaas/cybernetisk.tf
@@ -68,6 +68,17 @@ resource "openstack_networking_secgroup_rule_v2" "http" {
 	security_group_id = "${openstack_networking_secgroup_v2.web.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "ssh" {
+	direction         = "ingress"
+	ethertype         = "IPv4"
+	protocol          = "tcp"
+	port_range_min    = 22
+	port_range_max    = 22
+	remote_ip_prefix  = "0.0.0.0/0"
+	security_group_id = "${openstack_networking_secgroup_v2.minion.id}"
+}
+
+
 resource "openstack_networking_secgroup_rule_v2" "https" {
 	direction         = "ingress"
 	ethertype         = "IPv4"
