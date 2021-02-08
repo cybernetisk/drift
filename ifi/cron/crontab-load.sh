5ce0745b4341848006d16616f4d865ada3c8a824
Mats Schjølberg <matsasc@cyb.no>
2021-02-08 16:34:26 +0100

    Update crontab load script

--- ifi/cron/crontab-load.sh
+++ ifi/cron/crontab-load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(id -nu) != cyb ] || [ $(hostname) != vor.ifi.uio.no ]; then
+if [ $(id -nu) != cyb ] || [ $(hostname) != nidur.ifi.uio.no ]; then
     "Must be user 'cyb' running on 'vor.ifi.uio.no' to use this script"
     exit 1
 fi
